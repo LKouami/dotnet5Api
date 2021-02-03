ebf48389de36479824ded31def51da390b1bb131
rkouami <rkouami@outlook.com>
2021-02-03 17:07:33 +0000

    End of section 5

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,20 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import { CommonModule } from '@angular/common';  
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule} from '@angular/forms' 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent
+  ],
+  exports:[
+    NavComponent,
+    RegisterComponent,
+    HomeComponent,
+    FormsModule
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    CommonModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    BsDropdownModule.forRoot(),
+    FormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
